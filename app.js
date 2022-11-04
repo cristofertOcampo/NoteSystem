2de2c6c78cd843b26ddb29c8c742202a72d068f6
Cristofert Ocampo <ocampocristofert@gmail.com>
2022-11-04 12:44:00 -0500

    Se maqueto el formulario y selecciono inputs

---
+++ app.js
@@ -0,0 +1,17 @@
+const note1 = document.getElementById("txt-note1");
+const note2 = document.getElementById("txt-note2");
+const note3 = document.getElementById("txt-note3");
+const send = document.getElementById("btn-send");
+
+let Note1 = parseFloat(note1.value);
+let Note2 = parseFloat(note2.value);
+let Note3 = parseFloat(note3.value);
+
+
+
+send.addEventListener('click', ()=>{
+
+})
+
+
+
