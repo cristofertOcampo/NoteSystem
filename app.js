f4bde42f74602412b1ffc345096e6f30c49f731b
Cristofert Ocampo <ocampocristofert@gmail.com>
2022-11-04 13:30:21 -0500

    Se le Hicieron validaciones de las notas para saber si el usuario es Exelente, Bueno , perdio pero puede recuperar o perdio y debe recuperar tambien redondea la nota final

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
+const nameUser = document.getElementById("txt-name");
 const note1 = document.getElementById("txt-note1");
 const note2 = document.getElementById("txt-note2");
 const note3 = document.getElementById("txt-note3");
 const send = document.getElementById("btn-send");
+const final = document.getElementById("final-note");
 
-send.addEventListener('click', ()=>{
+send.addEventListener("click", () => {
+  let Note1 = parseFloat(note1.value);
+  let Note2 = parseFloat(note2.value);
+  let Note3 = parseFloat(note3.value);
 
-let Note1 = parseFloat(note1.value);
-let Note2 = parseFloat(note2.value);
-let Note3 = parseFloat(note3.value);
+  if (Note1 <= 5 && Note1 >= 1 && Note2 <= 5 && Note2 >= 1 && Note3 <= 5 && Note3 >= 1
+  ) {
+    let result = 0.3 * Note1 + 0.3 * Note2 + 0.4 * Note3;
 
+    if (result >= 4.5 && result <= 5) {
+      final.textContent = `El estudiante ${nameUser.value}, tienen como nota final ${result.toFixed(1)} y es considerado Excelente.`;
+    }if(result >= 3.5 && result <= 4.5){
+      final.textContent = `El estudiante ${nameUser.value}, tienen como nota final ${result.toFixed(1)} y es considerado Bueno.`;
+    }if(result >= 2 && result <= 3.4){
+      final.textContent = `El estudiante ${nameUser.value}, tienen como nota final ${result.toFixed(1)} perdido la materia pero contará con la oportunidad de recuperar.`;
+    }else{
+        final.textContent = `El estudiante ${nameUser.value}, tienen como nota final ${result.toFixed(1)} perdido la materia y deberá repetirla.`;
+    }
 
-if(Note1 <= 5 && Note1 >= 1 && Note2 <= 5 && Note2 >= 1 && Note3 <= 5 && Note3 >= 1){
-  let result = 0.30 * Note1 + 0.30 * Note2 + 0.40 * Note3;
-  
-  console.log(result);
-}else{
-  console.log('Error');
-}
-
-})
-
-
-
+    console.log(result.toFixed(2));
+  } else {
+    console.log("Error");
+  }
+});
